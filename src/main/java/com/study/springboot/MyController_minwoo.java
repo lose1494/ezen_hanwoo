4868ad7790076bbb0adc1898874d9650892e7b14
alsdn2514 <tmflr84646@gmail.com>
2022-09-28 12:18:22 +0900

    commit

--- src/main/java/com/study/springboot/MyController_minwoo.java
+++ src/main/java/com/study/springboot/MyController_minwoo.java
@@ -1,8 +1,17 @@
 package com.study.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController_minwoo {
 
+	@RequestMapping("/product/search_result")
+	public String search_result (Model model) {
+		model.addAttribute("mainPage","product/search_result.jsp");
+		return "index";
+	}
+	
+	
 }
