012717002f3d1cfb114556a19aad610637ba056f
lose1494 <gyori0504@gmail.com>
2022-09-28 12:51:20 +0900

    ㅁㄴㅇㅁㄴㅇ

--- src/main/java/com/study/springboot/MyController_minwoo.java
+++ src/main/java/com/study/springboot/MyController_minwoo.java
@@ -13,17 +13,6 @@ public class MyController_minwoo {
 		return "index";
 	}
 	
-<<<<<<< HEAD
-	
-	
-	
-	
-	
-	
-	
-	
-	
-=======
 	@RequestMapping("/mypage/mypage_cart")
 	public String mypage_cart (Model model) {
 		model.addAttribute("mainPage","mypage/mypage_cart.jsp");
@@ -47,7 +36,7 @@ public class MyController_minwoo {
 		model.addAttribute("mainPage","member/join2.jsp");
 		return "index";
 	}
->>>>>>> minwoo
+
 	
 	
 }
