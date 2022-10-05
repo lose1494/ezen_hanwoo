54425e908d197d6a887abf1775bd8c79af2e835f
asfmnaew <poerd_@naver.com>
2022-10-05 10:35:38 +0900

    sql 수정

--- src/main/resources/static/ezenhanwoo.sql
+++ src/main/resources/static/ezenhanwoo.sql
@@ -9,25 +9,26 @@ create table ezen_users(
     users_email_receive   number(1),	--0: 비수신 1: 수신   
     users_phone         varchar2(20),
     users_phone_receive   number(1),	--0: 비수신 1: 수신
-    users_birth_date      date,
-    users_point    varchar2(20)
+    users_birth_date      date
 );
-​
+
 drop sequence ezen_users_seq;
 create sequence ezen_users_seq;
 
 INSERT INTO ezen_users
-VALUES (ezen_users_seq.nextval, 'hong', '1234', '홍길동', 'test1@gmail.com', 0, '01011111111', 0, '2000-01-01', 5000);
-​
+VALUES (ezen_users_seq.nextval, 'hong', '1234', '홍길동', 'test1@gmail.com', 0, '01011111111', 0, '2000-01-01');
+
 --상품
 drop table product;
 create table product(
     product_idx number(4) primary key,
     product_name varchar2(20),
+    product_image varchar2(40),
     product_price varchar2(20),
     product_category varchar2(20),
-    product_explanation varchar2(40),
-    product_gram varchar2(20)
+    product_gram varchar2(20),
+    product_packaging varchar2(20),
+    product_explanation varchar2(60)   
 );
 drop sequence product_seq;​
 create sequence product_seq;
@@ -42,12 +43,13 @@ create table product_qna(
     qna_date date,   
     qna_secret   number(1), --0: 비밀글 설정안함 1: 비밀글 설정
     qna_pw varchar2(20),
-    product_idx number(4), 
+    qna_status varchar2(20),
+    product_idx number(4)
 );
-​
+
 drop sequence product_qna_seq;​
 create sequence product_qna_seq;
-​
+
 --상품 문의 답변
 drop table product_qna_reply;
 create table product_qna_reply(
@@ -57,7 +59,7 @@ create table product_qna_reply(
     reply_date date,   
     qna_idx number(4)
 );
-​
+
 drop sequence product_qna_reply_seq;​
 create sequence product_qna_reply_seq;
 ​
@@ -70,9 +72,9 @@ create table product_review(
     review_content varchar2(300),
     review_date date, 
     review_star_rating number(4),
-    product_idx number(4)
+    product_idx number(4),
 );
-​
+
 drop sequence product_review_seq;
 create sequence product_review_seq;
 ​
@@ -96,7 +98,7 @@ create table Notice(
     notice_content varchar2(300),
     notice_date date    
 );
-​
+
 drop sequence Notice_seq;​
 create sequence Notice_seq;
 ​
@@ -109,7 +111,7 @@ create table one2one(
     one2one_content varchar2(300),
     one2one_date date  
 );
-​
+
 drop sequence one2one_seq;​
 create sequence one2one_seq;
 ​
@@ -123,36 +125,46 @@ create table one2one_answer(
     answer_date date, 
     one2one_answer_idx number(4)
 );
-​
+
 drop sequence one2one_answer_seq;​
 create sequence one2one_answer_seq;
 ​
 --주문
-drop table order;
+drop table orderlist;
 create table orderlist(
 order_idx number(4) primary key,
 order_no number(8),
 order_users_id varchar2(20),
-order_product_name varchar2(20),
 order_total_price number(4),
 order_comment varchar2(100),
 order_date date,
-order_address varchar(100),
+order_recipient varchar2(20),
+order_address1 varchar(20),
+order_address2 varchar(100),
+order_address3 varchar(100),
 order_phone number(20)
 );
-​
-drop sequence order_seq;​
-create sequence order_seq;
+
+drop sequence orderlist_seq;​
+create sequence orderlist_seq;
+
+--주문 상세
+drop table orderDetail;
+create table orderDetail(
+detail_idx number(4) primary key,
+order_idx number(4),
+prouct_idx number(4)
+);
 ​
 --장바구니
 drop table cart;
 create table cart(
 cart_idx number(4) primary key,
-users_idx number(4),
+cart_users_id varchar(20),
 cart_product_name varchar(20),
 cart_count number(4)
 );
-​
+
 drop sequence cart_seq;
 create sequence cart_seq;
 ​
@@ -164,8 +176,28 @@ list_users_id varchar(20),
 list_product_name varchar(20),
 list_count number(4)
 );
-​
+
 drop sequence wishlist_seq;​
 create sequence wishlist_seq;
+
+--적립금
+drop table point;
+create table point(
+point_idx number(4) primary key,
+point_users_id varchar(20),
+point_start_date date,
+point_point number(8),
+point_end_date date,
+point_content varchar(60)
+);
+
+drop sequence point_seq;​
+create sequence point_seq;
+
+insert into point
+values( point_seq.nextval, 'hong', sysdate, +5000, sysdate+365, '회원가입');
+
+insert into point
+values( point_seq.nextval, 'hong', sysdate, +500, sysdate+365, '테스트');
 
 commit;
